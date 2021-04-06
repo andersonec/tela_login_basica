de21696b222f1f4bb1b0d4ef015cc9e302f2f888
AndersonSantos <anderson.santos@dcomp.ufs.br>
2021-04-06 11:15:32 -0300

    Adicionado validaçao de email

--- treat.js
+++ treat.js
@@ -14,3 +14,25 @@ password.addEventListener('focus', ()=>{
 password.addEventListener('blur', ()=>{
     password.style.borderColor = "#ccc";
 });
+
+function validaEmail(field){
+    usuario = field.value.substring(0, field.value.indexOf("@"));
+    dominio = field.value.substring(field.value.indexOf("@") + 1, field.value.length);
+
+    if((usuario.length >= 1) &&
+    (dominio.length >= 3) &&
+    (usuario.search("@") == -1) &&
+    (dominio.search("@") == -1) &&
+    (usuario.search(" ") == -1) &&
+    (dominio.search(" ") == -1) &&
+    (dominio.search(".") != -1) &&
+    (dominio.indexOf(".") >= -1) &&
+    (dominio.lastIndexOf(".") < dominio.length -1)){
+        document.getElementById("msgemail").innerHTML="<font color='green'>OK.</font>";
+        alert("Email válido.");
+    }
+    else{
+        document.getElementById("msgemail").innerHTML="<font color='red'>NO.</font>";
+        alert("Email inválido.");
+    }
+}
